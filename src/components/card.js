da13fc34323c70831a6caca784291b4914008a16
icarus <ellishogan95@gmail.com>
2019-08-13 19:59:46 -0400

    fixed

--- src/components/card.js
+++ src/components/card.js
@@ -1,16 +1,11 @@
 import React from 'react';
 const Card = (props) => {
 
-
-        this.state = {
-            classes = ['throbber-inner', props.pulsar]
-        }
-    
-
+        let classes = ['throbber-inner', props.pulsar];    
         return (
             <div className="col-7 step">
                 <div className="throbber">
-                    <div className={classNames.join(' ')}></div>
+                    <div className={classes.join(' ')}></div>
                 </div>
                 <div>
                     <h4 className="description">
